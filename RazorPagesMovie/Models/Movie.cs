68780c302d25e9147404dd9b79111213f892d53e
jose junior <jose.junior@digitalfactory.co.ao>
2021-05-12 00:07:14 +0100

    Adicionar arquivos de projeto.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "Decimal(18,2)")]
+        public Decimal Price { get; set; }
+
+        [Range(1, 100)]
+        public string Rating { get; set; }
+
+    }
+}
